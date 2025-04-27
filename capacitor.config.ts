51894990e061c1b2d2aa5e834b644dd43bc1c740
Darius Bordeanu <bordeanu96darius@gmail.com>
2025-04-27 21:35:04 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'trasaturi_personalitate_react_ionic',
+  webDir: 'dist'
+};
+
+export default config;
